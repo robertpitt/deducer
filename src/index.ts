e8ce5261d6a95c0ec9b3cca91ae343a2d876e435
Robert Pitt <robertpitt1988@gmail.com>
2020-02-06 12:56:39 +0000

    fix build

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { get, set, Path } from 'object-path'
+import * as path from 'object-path'
 import { pipe } from './utilities'
 /**
  *
@@ -6,7 +6,7 @@ import { pipe } from './utilities'
 export type DestinationKey = string
 export type KVObject = { [key: string]: any }
 export type InputModel = any[] | KVObject
-export type Element = [Path, Path, ...Function[]]
+export type Element = [path.Path, path.Path, ...Function[]]
 export type ElementMap = Element[]
 
 /**
@@ -15,8 +15,10 @@ export type ElementMap = Element[]
 export const transform = (input: InputModel, map: ElementMap): KVObject => {
   const output = {}
   map.forEach(([source, target, ...transformFns]) => {
-    const args = Array.isArray(source) ? source.map(key => get(input, key)) : get(input, source)
-    set(output, target, pipe(...transformFns)(args))
+    const args = Array.isArray(source)
+      ? source.map(key => path.get(input, key))
+      : path.get(input, source)
+    path.set(output, target, pipe(...transformFns)(args))
   })
   return output
 }
