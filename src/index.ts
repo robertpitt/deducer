05727f3e9ea886d3258ac4c35812054db9d20cf0
Robert Pitt <robertpitt1988@gmail.com>
2020-02-06 20:13:40 +0000

    100% Test Coverage, switched to better pipeline that supports types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { pipe } from 'pipe-and-compose'
 import * as invariant from 'invariant'
 import * as path from 'object-path'
 
@@ -8,13 +9,14 @@ import * as path from 'object-path'
 export type DestinationKey = string
 export type KVObject = { [key: string]: any }
 export type InputModel = any[] | KVObject
-export type Element = [path.Path, path.Path, ...Function[]]
+export type AnyFunction = (arg: any) => any
+export type Element = [path.Path, path.Path, ...AnyFunction[]]
 export type ElementMap = Element[]
 
 /**
  * Helper function used to curry function call results into the next function (RTL not LTR)
  */
-export const pipe = (...fns: Function[]) => (args: any[]) => fns.reduce((arg, fn) => fn(arg), args)
+// export const pipe = (...fns: Function[]) => (args: any[]) => fns.reduce((arg, fn) => fn(arg), args)
 
 /**
  * Transform an input based on a map of transform actions
@@ -27,11 +29,20 @@ export const transform = (input: InputModel, map: ElementMap): KVObject => {
   invariant(map instanceof Array, 'Map must be an array of transforms')
 
   // Use Reduce
-  return map.reduce((o, [srcKey, dstKey, ...transforms]) => {
-    const args = Array.isArray(srcKey)
-      ? srcKey.map(key => path.get(input, key))
-      : path.get(input, srcKey)
-    path.set(o, dstKey, pipe.apply(void 0, transforms)(args))
+  return map.reduce((o, [sourcePath, targetPath, ...transforms]) => {
+    // Create a new execution stack that will process the current transfomration
+    const executor = pipe(
+      () =>
+        Array.isArray(sourcePath)
+          ? sourcePath.map(key => path.get(input, key))
+          : path.get(input, sourcePath),
+      ...transforms
+    )
+
+    // Execute the pipline and set the result to the target key
+    path.set(o, targetPath, executor())
+
+    // Return the
     return o
-  }, {})
+  }, Object.create({}))
 }
--- test/data-transformer.test.ts
+++ test/data-transformer.test.ts
@@ -1,5 +1,4 @@
 import { transform, ElementMap } from '../src'
-import { pipe } from '../src/utilities'
 
 /**
  * Dummy test
@@ -86,7 +85,107 @@ describe('Transformer', () => {
     })
   })
 
-  describe('Mutations', () => {
-    it('should be able to pipe many functions against a value', () => {})
+  describe('Examples', () => {
+    it('should fully convert an array into a strucuture object with nested properties', () => {
+      // Helper for test
+      const trim = (s: any) => (typeof s === 'string' ? s.trim() : s)
+      const toUpper = (s: any) => (typeof s === 'string' ? s.toUpperCase() : s)
+      const toDate = ([d, t]: [string, string]) => {
+        return new Date(2018, 1, 1, 0, 0, 0)
+      }
+
+      // Input
+      const input = [
+        'N',
+        'PRD001',
+        'ABCDE',
+        'JONES   ',
+        'MR  ',
+        'J',
+        '  1',
+        '07:08',
+        '13/05/2018',
+        '001',
+        'CONFIRMED  ',
+        '  7.97 ',
+        '10.99',
+        '16/05/2018',
+        '18:00',
+        ' ABc123   ',
+        'QI',
+        'BLuE      ',
+        'VOLkSwAGeN',
+        'TIhuaN SE ',
+        '  ',
+        '01234567890',
+        '01234567890',
+        '07/00',
+        '',
+        '     ',
+        'u2192     ',
+        '-                   ',
+        '              ',
+        'Ax246 ',
+        '',
+        'Y',
+        ''
+      ]
+
+      // Transform
+      const result = transform(input, [
+        [1, 'booking.products', trim, toUpper],
+        [2, 'booking.reference', trim, toUpper],
+        [6, 'booking.noPassengers', Number],
+        [[8, 7], 'booking.startDateTime', toDate],
+        [[13, 14], 'booking.endDateTime', toDate],
+        [10, 'booking.statusCode', trim, toUpper],
+        [11, 'booking.priceExclTax', trim, Number],
+        [12, 'booking.price', trim, Number],
+        [3, 'customer.surname', trim, toUpper],
+        [4, 'customer.title', trim, toUpper],
+        [5, 'customer.initial', trim, toUpper],
+        [15, 'customer.vehicle.model', trim, toUpper],
+        [16, 'customer.vehicle.registration', trim, toUpper],
+        [17, 'customer.vehicle.color', trim, toUpper],
+        [18, 'customer.vehicle.make', trim, toUpper],
+        [21, 'customer.contact.primary', trim],
+        [22, 'customer.contact.secondary', trim],
+        [26, 'flight.inboundCode', trim, toUpper],
+        [29, 'flight.outboundCode', trim, toUpper]
+      ])
+
+      //Ouput
+      expect(result).toMatchObject({
+        booking: {
+          products: 'PRD001',
+          reference: 'ABCDE',
+          noPassengers: 1,
+          startDateTime: new Date('2018-02-01T00:00:00.000Z'),
+          endDateTime: new Date('2018-02-01T00:00:00.000Z'),
+          statusCode: 'CONFIRMED',
+          priceExclTax: 7.97,
+          price: 10.99
+        },
+        customer: {
+          surname: 'JONES',
+          title: 'MR',
+          initial: 'J',
+          vehicle: {
+            model: 'ABC123',
+            registration: 'QI',
+            color: 'BLUE',
+            make: 'VOLKSWAGEN'
+          },
+          contact: {
+            primary: '01234567890',
+            secondary: '01234567890'
+          }
+        },
+        flight: {
+          inboundCode: 'U2192',
+          outboundCode: 'AX246'
+        }
+      })
+    })
   })
 })
