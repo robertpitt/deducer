b2f39412c5be132e0e7161c742d2fe0b6e5299fc
Robert Pitt <robertpitt1988@gmail.com>
2020-02-06 23:12:57 +0000

    refactor(project): upgraded all dependancies to the latest version

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
 
@@ -23,7 +22,10 @@ export default {
     // Allow json resolution
     json(),
     // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript({
+      useTsconfigDeclarationDir: true,
+      objectHashIgnoreUnknownHack: true,
+    }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
